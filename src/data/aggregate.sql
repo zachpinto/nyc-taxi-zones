65f8f8e34c10fc3e3609317201557aa55e1148ed
Zachary TW Pinto <zachwalker.pinto@gmail.com>
2024-02-05 00:53:46 -0500

    sql aggregation script

---
+++ src/data/aggregate.sql
@@ -0,0 +1,23 @@
+CREATE TABLE weekly_zone_aggregates AS
+SELECT
+  EXTRACT(YEAR FROM CAST(tpep_pickup_datetime AS TIMESTAMP)) AS year,
+  EXTRACT(WEEK FROM CAST(tpep_pickup_datetime AS TIMESTAMP)) AS week,
+  "PULocationID",
+  SUM("passenger_count") AS total_passengers,
+  SUM("trip_distance") AS total_distance,
+  SUM("total_amount") AS total_amount
+FROM
+  taxi_trips
+WHERE
+  CAST(tpep_pickup_datetime AS TIMESTAMP) BETWEEN '2018-01-01 00:00:00' AND '2022-12-31 23:59:59'
+GROUP BY
+  year,
+  week,
+  "PULocationID"
+ORDER BY
+  year,
+  week,
+  "PULocationID";
+
+
+COPY weekly_zone_aggregates TO '/Users/pintoza/Desktop/dev/data-science/taxi-demand-forecast/data/interim/weekly_zone_aggregates.csv' DELIMITER ',' CSV HEADER;
